0dbd5efcaf4352337d545199961e8655e45fb22d
Mateusz Migas <mateuszmigas.dev@gmail.com>
2022-12-15 19:38:36 +0100

    improvement

--- strong-api.ts
+++ strong-api.ts
@@ -17,10 +17,12 @@ type ProxyClient<T extends Record<string | symbol | number, any>> = {
   [K in keyof T]: T[K] extends (...args: any[]) => any ? T[K] : never;
 };
 
-const createProxyClient = <T extends {}>() => {
+const createProxyClient = <T extends {}>(
+  resolver: (method: string, args: any[]) => Promise<any>
+) => {
   const client = {
     someExtraClientMethod() {
-      console.log("extra method");
+      console.log("extra client method");
     },
   };
   const handler = {
@@ -28,18 +30,19 @@ const createProxyClient = <T extends {}>() => {
       if (client.hasOwnProperty(prop)) {
         return client[prop];
       }
-
-      return (...args: any[]) => {
-        //todo: calling server logic, rest/soap, etc
-        return api[prop](...args);
-      };
+      return (...args: any[]) => resolver(prop, args);
     },
   };
 
   return new Proxy(client, handler) as ProxyClient<T> & typeof client;
 };
 
-const client = createProxyClient<ServerApi>();
+//http/soap/etc resolver
+const resolver = (method: string, args: any[]): Promise<any> => {
+  return api[method](...args);
+};
+
+const client = createProxyClient<ServerApi>(resolver);
 client.someExtraClientMethod();
 console.log("client gets user: ", await client.getUser(12));
 client.updateUser({ id: 1, name: "John" });
